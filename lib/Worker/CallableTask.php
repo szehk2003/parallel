ef7d87339c37473215318e1f5e91f221a19b0efa
Roy Sze <roy.sze@optitable.com>
2019-06-17 09:46:21 +0800

    fix

--- lib/Worker/CallableTask.php
+++ lib/Worker/CallableTask.php
@@ -2,6 +2,8 @@
 
 namespace Amp\Parallel\Worker;
 
+use Yii;
+
 /**
  * Task implementation dispatching a simple callable.
  */
@@ -32,6 +34,24 @@ final class CallableTask implements Task
         if (\is_array($this->callable) && ($this->callable[0] ?? null) instanceof \__PHP_Incomplete_Class) {
             throw new \Error('When using a class instance method as a callable, the class must be autoloadable');
         }
+
+        (function () {
+            require_once(__DIR__ . '/../../../../autoload.php');
+            require_once(__DIR__ . '/../../../../yiisoft/yii2/Yii.php');
+            require_once(__DIR__ . '/../../../../smarty/smarty/libs/Smarty.class.php');
+
+            $config = require __DIR__ . '/../../../../../config/console.php';
+
+            $application = new \yii\console\Application($config);
+
+            $renderer = $application->getView()->renderers['tpl'];
+
+            if(is_array($renderer)) {
+                Yii::$app->getView()->renderers['tpl'] = $renderer = Yii::createObject($renderer);
+            }
+
+            $renderer->getSmarty()->addPluginsDir(dirname(__DIR__) . '/extensions/smarty/plugins');
+        })();
 
         return ($this->callable)(...$this->args);
     }
